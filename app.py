41686d832ef38843be738da0b19c1447f8ce15de
RANJANI586 <ranjaniramesh586@gmail.com>
2026-04-25 07:12:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def calculate_bill(units):
     if units <= 100:
         amount = units * 1.5
     elif units <= 200:
-        amount = (100 * 1.5) + (units - 100) * 2.5
+        amount = (100 * 1.5) + (units - 100)*2.5
     elif units <= 300:
         amount = (100 * 1.5) + (100 * 2.5) + (units - 200) * 4
     else:
